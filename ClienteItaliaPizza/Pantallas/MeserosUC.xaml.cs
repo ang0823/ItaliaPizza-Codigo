d5fd000ae10eeb9a3600a8c72a82f6aa23874322
irasemcai <36616732+irasemcai@users.noreply.github.com>
2020-05-12 17:18:52 -0500

    Agrego usercontrol´s de Meseros y Call Center

---
+++ ClienteItaliaPizza/Pantallas/MeserosUC.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace ClienteItaliaPizza.Pantallas
+{
+    /// <summary>
+    /// Lógica de interacción para MeserosUC.xaml
+    /// </summary>
+    public partial class MeserosUC : UserControl
+    {
+        public MeserosUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
