f0d9275cd4e9c9e1237a67f910464e686497d795
ang0823 <angelsanchez934@gmail.com>
2020-05-20 02:17:46 -0500

    Se implemento el funcionamiento para que se puedan registrar empleados en el servidor

--- ClienteItaliaPizza/RegistroEmpleados.xaml.cs
+++ ClienteItaliaPizza/RegistroEmpleados.xaml.cs
@@ -5,7 +5,6 @@ using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace ClienteItaliaPizza
 {
