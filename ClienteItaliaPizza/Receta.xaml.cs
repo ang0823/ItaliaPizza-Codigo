2a90b7546ad73d592a4beed554e525e6146ddaa2
irasemcai <36616732+irasemcai@users.noreply.github.com>
2020-06-11 15:27:45 -0500

    Implemento Registrar receta

--- ClienteItaliaPizza/Receta.xaml.cs
+++ ClienteItaliaPizza/Receta.xaml.cs
@@ -3,6 +3,8 @@ using ClienteItaliaPizza.Validacion;
 using System.Collections.Generic;
 using System.ServiceModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace ClienteItaliaPizza
@@ -14,13 +16,14 @@ namespace ClienteItaliaPizza
     {
         protected object recetaExistente; //creo esta clase temporalmente para VALIDAR si la ventna se llama con un objeto receta o no
         protected CuentaUsuario1 cuenta = new CuentaUsuario1();
-
+        InstanceContext context;
         List<Ingrediente> Ingredientes = new List<Ingrediente>();
-        List<Provision> Provisiones = new List<Provision>();
+        
         public Receta(CuentaUsuario1 cuentaUsuario)
-        {
+        {            
             InitializeComponent();
-          //  cuenta = cuentaUsuario;
+            dataGridIngredientes.ItemsSource = Ingredientes;
+            cuenta = cuentaUsuario;
         }
 
         public Receta(CuentaUsuario1 cuentausuario, object recetaexistente)
@@ -31,10 +34,8 @@ namespace ClienteItaliaPizza
         }
 
         private void TextBoxNombreReceta_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            Validador Validacion = new Validador();
-            bool resultadoValidacion = Validacion.validarSoloLetrasConAcentos(e.Text);
-            if (resultadoValidacion == false)
+        {                      
+            if (Validador.validarLetrasConAcentosYNumeros(e.Text) == false)
             {
                 e.Handled = true;
             }
@@ -42,9 +43,7 @@ namespace ClienteItaliaPizza
 
         private void TextBoxPorciones_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            Validador Validacion = new Validador();
-            bool resultadoValidacion = Validacion.validarSoloNumeros(e.Text);
-            if (resultadoValidacion == false)
+            if (Validador.validarSoloNumeros(e.Text) == false )
             {
                 e.Handled = true;
             }
@@ -52,42 +51,33 @@ namespace ClienteItaliaPizza
 
         private void TextBoxProcedimiento_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            Validador Validacion = new Validador();
-            bool resultadoValidacion = Validacion.validarLetrasConAcentosYNumeros(e.Text);
-            if (resultadoValidacion == false)
+            if (Validador.validarLetrasConAcentosYNumeros(e.Text) == false)
             {
                 e.Handled = true;
             }
-
         }
 
         private void ButtonAceptar_Click(object sender, RoutedEventArgs e)
         {
-            InstanceContext context = new InstanceContext(this);               
-            RegistrarRecetaClient registrarRecetaClient = new RegistrarRecetaClient(context);
-            Servicio.Receta receta = new Servicio.Receta();
-            receta.nombreReceta = textBoxNombreReceta.Text;
-            receta.porciones = FuncionesComunes.ParsearADouble(textBoxPorciones.Text);
-            receta.procedimiento = textBoxProcedimiento.Text;
-
-            receta.Ingrediente = new Ingrediente[1];
-            Ingredientes.CopyTo(receta.Ingrediente);
-
-            receta.Provision = new Provision[1];
-            Provisiones.CopyTo(receta.Provision);
-
-            Producto producto = new Producto();
-            producto.nombre = "Pizza hawaiana";
-            producto.descripcion = "sencilla";
-            producto.precioUnitario = 10.00;
-            producto.activado = true;
-            producto.restricciones = "loquesea";
-
-            Categoria categoria = new Categoria();
-            categoria.categoria = "Pizza";
-
-          //  registrarRecetaClient.RegistrarReceta(receta, producto , categoria);
-
+             context = new InstanceContext(this);               
+             RegistrarRecetaClient registrarRecetaClient = new RegistrarRecetaClient(context);
+            try
+            {
+             Servicio.Receta receta = new Servicio.Receta();
+             receta.nombreReceta = textBoxNombreReceta.Text;
+             receta.porciones = FuncionesComunes.ParsearADouble(textBoxPorciones.Text);
+             receta.procedimiento = textBoxProcedimiento.Text;
+            receta.activado = true;
+             
+            //receta.Ingrediente = new Ingrediente[1];
+            //Ingredientes.CopyTo(receta.Ingrediente);
+             var array = Ingredientes.ToArray();
+             registrarRecetaClient.RegistrarReceta(receta, array);
+            }
+            catch (CommunicationException)
+            {
+                FuncionesComunes.MostrarMensajeDeError("no hay conexion");
+            }                         
         }
 
         private void ButtonCancelar_Click(object sender, RoutedEventArgs e)
@@ -116,15 +106,102 @@ namespace ClienteItaliaPizza
                 e.Handled = true;
             }
         }
-
-        public void RegistrarRecetaRespuesta(string mensaje)
+       
+        public void RespuestaRR(string mensaje)
         {
             MessageBox.Show(mensaje);
         }
 
-        public void RespuestaRR(string mensaje)
+        private void DataGridIngredientes_RowEditEnding(object sender, System.Windows.Controls.DataGridRowEditEndingEventArgs e)
         {
-            throw new System.NotImplementedException();
+          
+        }
+
+        private void DataGridIngredientes_CellEditEnding(object sender, System.Windows.Controls.DataGridCellEditEndingEventArgs e)
+        {           
+            if (e.EditAction == DataGridEditAction.Commit)
+            {
+                var n = Ingredientes.Count;
+                var columna = e.Column as DataGridBoundColumn;
+                if (columna != null)
+                {
+                    var nombreColumna = (columna.Binding as Binding).Path.Path;
+                    if (nombreColumna == "nombre")
+                    {
+                        var celdaEditada = e.EditingElement as TextBox;
+
+                        if (celdaEditada.Text.Length == 0)
+                        {
+                            FuncionesComunes.MostrarMensajeDeError("No se aceptan espacios vacios.");
+                        }                       
+                    }
+                    if(nombreColumna == "cantidad")
+                    {
+                        var entradaCantidad = e.EditingElement as TextBox;
+                        if (entradaCantidad.Text.Length == 0 )
+                        {
+                            FuncionesComunes.MostrarMensajeDeError("No se aceptan espacios vacios.");
+                        }                       
+                    }
+                    if(nombreColumna == "peso")
+                    {
+                        var entradaPeso = e.EditingElement as TextBox;
+                        if (entradaPeso.Text.Length == 0)
+                        {
+                            FuncionesComunes.MostrarMensajeDeError("No se aceptan espacios vacios.");
+                        }                       
+                    }
+                    if(nombreColumna == "unidad")
+                    {
+                        var entradaUnidad = e.EditingElement as TextBox;
+                        if (entradaUnidad.Text.Length == 0)
+                        {
+                            FuncionesComunes.MostrarMensajeDeError("No se aceptan espacios vacios.");
+                        }                       
+                    }
+                    if(nombreColumna == "costoPorUnidad")
+                    {
+                        var entradaCostoPorUnidad = e.EditingElement as TextBox;
+                        if (entradaCostoPorUnidad.Text.Length == 0)
+                        {
+                            FuncionesComunes.MostrarMensajeDeError("No se aceptan espacios vacios.");
+                            
+                        }                        
+                    }
+                }
+            }
+
+        }
+
+        private void DataGridIngredientes_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+          if(dataGridIngredientes.CurrentColumn.Header.Equals("Ingrediente"))
+            {
+                if (Validador.validarLetrasConAcentosYNumeros(e.Text) == false)
+                    e.Handled = true;
+            }
+            if (dataGridIngredientes.CurrentColumn.Header.Equals("Cantidad"))
+            {
+                if (Validador.validarSoloNumeros(e.Text) == false)
+                    e.Handled = true;
+            }
+            if (dataGridIngredientes.CurrentColumn.Header.Equals("Peso"))
+            {
+                if (Validador.validarSoloNumerosConPunto(e.Text) == false)
+                    e.Handled = true;
+                    
+            }
+            if (dataGridIngredientes.CurrentColumn.Header.Equals("Unidad"))
+            {
+                if (Validador.validarSoloLetrasConAcentos(e.Text) == false)
+                    e.Handled = true;
+                
+            }
+            if (dataGridIngredientes.CurrentColumn.Header.Equals("Costo por Unidad"))
+            {
+                if (Validador.validarSoloNumerosConPunto(e.Text) == false)
+                    e.Handled = true;                
+            }
         }
     }
 }
